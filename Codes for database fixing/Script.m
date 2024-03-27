0a9143a658523f6afda3c80e7afa338b076f3212
Raphaël BOICHOT <63796796+Raphael-Boichot@users.noreply.github.com>
2024-03-27 12:38:35 +0100

    Delete Codes for database fixing directory

--- Codes for database fixing/Script.m
+++
@@ -1,9 +0,0 @@
-database='../Source database/ThermDoc23a.bib';
-Extract_missing_titles(database)
-Extract_missing_authors(database)
-Extract_missing_references(database)
-Extract_missing_keys(database)
-Extract_missing_dates(database)
-Sort_by_dates_and_fix_syntax(database)
-
-
