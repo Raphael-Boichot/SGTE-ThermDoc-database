711b179a687a0517dead6880bd021181c8dbb27a
Raphaël BOICHOT <63796796+Raphael-Boichot@users.noreply.github.com>
2024-04-17 21:14:12 +0200

    Update Script.m

--- Codes/Script.m
+++ Codes/Script.m
@@ -1,6 +1,4 @@
-clc                                     %Just clear the command window
-Search_for_elements('Mo')           		%search for pure Molybdenum metal only
-Search_for_elements('Al','Mo')      		%search for Al-Mo binary only
-Search_for_elements('Al','Cu','Mo') 		%search for Al-Cu-Mo ternary only
-Search_for_authors('Chatillon','Nuta')  	%search for Chatillon and Nuta as authors
+clc                                         %Just clear the command window
+Search_for_elements('Al','Cu','Mo') 		    %search for Al-Cu-Mo ternary only
+Search_for_authors('Chatillon','Nuta')  	  %search for Chatillon and Nuta as authors
 Search_for_keywords('Plutonium','Osmium') 	%search for paper containing Plutonium and Osmium in the title
