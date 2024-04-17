64ec9077a92765dc303a3457611c21bdb2d5d7b7
Raphaël BOICHOT <63796796+Raphael-Boichot@users.noreply.github.com>
2024-04-17 17:20:43 +0200

    Update Script.m

--- Codes/Script.m
+++ Codes/Script.m
@@ -1,4 +1,4 @@
-clc
+clc                                     %Just clear the command window
 Search_for_elements('Mo')           		%search for pure Molybdenum metal only
 Search_for_elements('Al','Mo')      		%search for Al-Mo binary only
 Search_for_elements('Al','Cu','Mo') 		%search for Al-Cu-Mo ternary only
