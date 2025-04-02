ab295f161c23f11e22d03bca220af19530571537
Dr. Raphaël BOICHOT <raphael.boichot@gmail.com>
2025-04-02 15:58:21 +0200

    Add files via upload

--- Codes/Service_folder/Service_script.m
+++ Codes/Service_folder/Service_script.m
@@ -3,7 +3,7 @@ disp('***************************************************************')
 disp('******Start of database formatting, this may take a while******')
 disp('***************************************************************')
 disp(' ')
-database_in='./Source_database/ThermDoc23b.bib';
+database_in='./Source_database/ThermDoc24b.bib';
 database_out='./Source_database/Sorted_database.bib';
 Extract_missing_titles(database_in)
 Extract_missing_authors(database_in)
